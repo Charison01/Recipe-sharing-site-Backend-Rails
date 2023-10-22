ff51558c7891e2ea0054729ae7d7bc6078ce201a
Charison01 <kelvinkamaru@gmail.com>
2023-10-22 16:59:43 +0300

    cloudinary configuration

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,11 @@
+development: 
+  cloud_name: "dvgfo1dcc"
+  api_key: "489973481747485"
+  api_secret: "***************************"
+  secure: true
+
+production:
+  cloud_name: "dvgfo1dcc"
+  api_key: "489973481747485"
+  api_secret: "***************************"
+  secure: true
