5b8223c23d5ba384daf357bfd978b784006d9e4f
Charison01 <kelvinkimaru@gmail.com>
2023-10-23 22:16:08 +0300

    user account update action added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
       end
     end
+
+    def update
+        if current_user.update(user_params)
+          render json: current_user, status: :ok
+        else
+          render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
   
     private
   
